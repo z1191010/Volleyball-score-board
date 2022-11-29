17ce2ff6e4729fee2825e97ccc6bdf814b031609
Kai.W <z1191010@gmail.com>
2022-11-29 22:34:27 +0800

    Initial Commit

---
+++ Volleyball scorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Volleyball scorer
+//
+//  Created by Kai on 2022/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
